550547946077eb8e9956cddd503271991ba31749
cypghost <hackghost2468@gmail.com>
2024-10-12 21:07:32 +0300

    Last Website Checkup

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,33 +1,33 @@
-import { Resend } from "resend";
-import { NextResponse } from "next/server";
-import ContactEmail from "@/email/Contact";
+// import { Resend } from "resend";
+// import { NextResponse } from "next/server";
+// import ContactEmail from "@/email/Contact";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request: Request) {
-  try {
-    const { name, email, message } = await request.json();
+// export async function POST(request: Request) {
+//   try {
+//     const { name, email, message } = await request.json();
 
-    const { data, error } = await resend.emails.send({
-      from: process.env.EMAIL_SENDER as string,
-      to: process.env.EMAIL_ADDR as string,
-      subject: "New Contact Request",
-      react: ContactEmail({ name, email, message }),
-    });
+//     const { data, error } = await resend.emails.send({
+//       from: process.env.EMAIL_SENDER as string,
+//       to: process.env.EMAIL_ADDR as string,
+//       subject: "New Contact Request",
+//       react: ContactEmail({ name, email, message }),
+//     });
 
-    console.log("Data:", data);
-    console.log("Error:", error);
+//     console.log("Data:", data);
+//     console.log("Error:", error);
 
-    if (error) {
-      return NextResponse.json({ error }, { status: 500 });
-    }
+//     if (error) {
+//       return NextResponse.json({ error }, { status: 500 });
+//     }
 
-    return NextResponse.json({ data }, { status: 200 });
-  } catch (error) {
-    console.error("Error:", error);
-    return NextResponse.json(
-      { error: "An error occurred while sending the email" },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json({ data }, { status: 200 });
+//   } catch (error) {
+//     console.error("Error:", error);
+//     return NextResponse.json(
+//       { error: "An error occurred while sending the email" },
+//       { status: 500 }
+//     );
+//   }
+// }
