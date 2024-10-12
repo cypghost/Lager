f19bd7f6a9e0cb8f296d523ffbc1748d6e977d9f
cypghost <hackghost2468@gmail.com>
2024-10-12 21:53:24 +0300

    Last Website Checkup

--- components/About/TeamMemberCard.tsx
+++ components/About/TeamMemberCard.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-// import { FiLinkedin, FiTwitter, FiFacebook } from "react-icons/fi";
-// import Image from "next/image";
-// import Link from "next/link";
 
 interface Socials {
   linkedin?: string;
@@ -16,51 +13,12 @@ interface TeamMemberCardProps {
   socials: Socials;
 }
 
-// const socialIcons = {
-//   linkedin: <FiLinkedin size={20} />,
-//   twitter: <FiTwitter size={20} />,
-//   facebook: <FiFacebook size={20} />,
-// };
-
-// const SocialIcons: React.FC<{ socials: Socials }> = ({ socials }) => (
-//   <div className="flex space-x-4 mt-4">
-//     {Object.entries(socials).map(([platform, url]) => (
-//       <Link
-//         key={platform}
-//         href={url!}
-//         aria-label={platform}
-//         target="_blank"
-//         rel="noopener noreferrer"
-//         className="text-primary hover:text-secondary transition-colors duration-300"
-//       >
-//         {socialIcons[platform as keyof Socials]}
-//       </Link>
-//     ))}
-//   </div>
-// );
-
-// Component for rendering a team member card
-const TeamMemberCard: React.FC<TeamMemberCardProps> = ({
-  name,
-  role,
-  // avatar,
-  // socials,
-}) => {
+const TeamMemberCard: React.FC<TeamMemberCardProps> = ({ name, role }) => {
   return (
     <div className="p-4 text-center bg-white rounded-lg shadow-md overflow-hidden">
-      {/* <div className="w-full h-48 md:h-64 lg:h-80 rounded-lg overflow-hidden">
-        <Image
-          src={avatar}
-          alt={name}
-          width={400}
-          height={400}
-          className="object-cover w-full h-full"
-        />
-      </div> */}
       <div className="p-6 flex flex-col items-center justify-center space-y-4">
         <h3 className="text-2xl font-semibold mt-2">{name}</h3>
         <p className="text-muted-foreground">{role}</p>
-        {/* <SocialIcons socials={socials} /> */}
       </div>
     </div>
   );
